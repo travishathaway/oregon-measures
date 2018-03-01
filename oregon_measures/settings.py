412d151d6360d07f827e6b5abfa5ceabce8d5909
Travis Hathaway <travis.j.hathaway@gmail.com>
2018-02-28 21:40:04 -0800

    updating settings

--- oregon_measures/settings.py
+++ oregon_measures/settings.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE = os.getenv('DATABASE') or 'postgresql://travishathaway@localhost:5432/om_2'
+DATABASE = os.getenv('DATABASE_URL') or 'postgresql://travishathaway@localhost:5432/om_2'
 DEBUG = os.getenv('DEBUG') or True
 
 SECRET_KEY = os.getenv('SECRET_KEY') or '92b2f9e0028b2b41cd7ce8ce94937ca2b767b486fc43d1c1'
